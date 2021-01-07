76f8348ed47738f7979f4b88cd78d91734add32d
nikolay <nikolayynayydenov@gmail.com>
2021-01-07 13:12:16 +0200

    add new autoloader and get rid of composer

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once('../vendor/autoload.php');
+//require_once('../vendor/autoload.php');
+require_once('autoloader.php');
 require_once('helpers.php');
 
 $config['database'] = require_once('../config/database.php');
