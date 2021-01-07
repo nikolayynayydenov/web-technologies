7c93e6b0103b2058eddb7ca9f175e6ccf8d6a9cf
nikolay <nikolayynayydenov@gmail.com>
2021-01-07 12:59:51 +0200

    update DB schema

--- database.sql
+++ database.sql
@@ -1,41 +1,76 @@
+-- --------------------------------------------------------
+-- Хост:                         localhost
+-- Версия на сървъра:            10.4.12-MariaDB-log - mariadb.org binary distribution
+-- ОС на сървъра:                Win64
+-- HeidiSQL Версия:              11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
+-- Дъмп на структурата на БД web-technologies
 CREATE DATABASE IF NOT EXISTS `web-technologies` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `web-technologies`;
 
+-- Дъмп структура за таблица web-technologies.attendance
+CREATE TABLE IF NOT EXISTS `attendance` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `faculty_number` mediumint(8) unsigned NOT NULL,
+  `logged_at` datetime DEFAULT NULL,
+  `event_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_attendance_event_id_events_id` (`event_id`),
+  CONSTRAINT `FK_attendance_event_id_events_id` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Изнасянето на данните беше деселектирано.
+
+-- Дъмп структура за таблица web-technologies.comments
 CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `content` text NOT NULL,
   `event_id` int(10) unsigned NOT NULL,
+  `is_visible` tinyint(4) DEFAULT 1,
+  `faculty_number` mediumint(8) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_event_id_events_id` (`event_id`),
   CONSTRAINT `FK_event_id_events_id` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- Изнасянето на данните беше деселектирано.
 
-
+-- Дъмп структура за таблица web-technologies.events
 CREATE TABLE IF NOT EXISTS `events` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `description` text DEFAULT NULL,
-  `date_time` datetime DEFAULT NULL,
-  `user_id` int(10) unsigned NOT NULL,
+  `start` datetime DEFAULT NULL,
+  `end` datetime DEFAULT NULL,
+  `teacher_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_user_id_users_id` (`user_id`),
-  CONSTRAINT `FK_user_id_users_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  KEY `FK_events_teacher_id_teachers_id` (`teacher_id`),
+  CONSTRAINT `FK_events_teacher_id_teachers_id` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
+-- Изнасянето на данните беше деселектирано.
 
-
-CREATE TABLE IF NOT EXISTS `users` (
+-- Дъмп структура за таблица web-technologies.teachers
+CREATE TABLE IF NOT EXISTS `teachers` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `faculty_number` mediumint(9) DEFAULT NULL,
-  `group` tinyint(3) unsigned DEFAULT NULL,
-  `major` varchar(255) DEFAULT NULL,
   `first_name` varchar(255) DEFAULT NULL,
   `middle_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Изнасянето на данните беше деселектирано.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
