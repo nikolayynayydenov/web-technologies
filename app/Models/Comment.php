a91d0925cd52df645c9f06178e2d8bda16c33516
ralitsa-d <raly_g_98@abv.bg>
2021-01-12 23:55:03 +0200

    added Comment.php and enterCommentIntoDB.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,4 +7,72 @@ use Core\Model;
 class Comment extends Model
 {
     protected static $table = 'comments';
+
+    private $textContent;
+    private $fn;
+    private $isVisible;
+
+    private $conn;
+
+    public function __construct($textContent, $fn, $isVisible){
+        $this->textContent = $textContent;
+        $this->fn = $fn;
+        $this->isVisible = $isVisible;
+        $this->conn = \Core\Database::getConnection();
+    }
+
+    public function getTextContent(){
+        return $this->textContent;
+    }
+    public function getFN(){
+        return $this->fn;
+    }
+    public function getIsVisible(){
+        return $this->isVisible;
+    }
+    public function getConn(){
+        return $this->conn;
+    }
+
+    public function exists(){
+        $query = [];
+        $sql = "SELECT * FROM comments WHERE textContent=:textContent AND fn=:fn";
+        $preparedStmt = $this->getConn()->prepare($sql);
+        try{
+            $preparedStmt->execute(["textContent" => $this->getTextContent(), "fn" => $this->getFN()]);
+        }catch(PDOException $e){
+            $errMsg = $e->getMessage();
+            $query = ["sucessfullyExecuted" => false, "errMessage" => $errMsg];
+            return $query;
+        }
+        $comments_assoc = $preparedStmt->fetch(PDO::FETCH_ASSOC);
+        if($comments_assoc){
+            $query = ["sucessfullyExecuted" => true, "commentExists" => true];
+            return $query;
+        }
+        else{
+            $query = ["sucessfullyExecuted" => true, "commentExists" => false];
+            return $query;
+        }
+    }
+
+    public function createComment(){
+        $sql = "INSERT INTO `comments`(`textContent`, `fn`, `isVisible`)
+                VALUE(:textContent, :fn, :isVisible)";
+        $preparedStmt = $this->getConn()->prepare($sql);
+        $query =[];
+
+        try {
+            $preparedStmt->execute(["textContent" => $this->getTextContent(), 
+                                    "fn" => $this->getFN(), 
+                                    "isVisible" => $this->getIsVisible]);
+            $query = ["successfullyExecuted" => true]; 
+        } catch(PDOException $e) {
+            $errMsg = $e->getMessage();
+            $query = ["successfullyExecuted" => false, "errMessage" => $errMsg];
+        }
+
+        return $query;
+    }
+
 }
---
+++ app/Models/enterCommentIntoDB.php
@@ -0,0 +1,64 @@
+<?php
+    include_once "Teacher.php";
+
+    $errors = [];
+    $result;
+
+    $conn = \Core\Database::getConnection();
+
+    function testInput($input) {
+        $input = trim($input);
+        $input = htmlspecialchars($input);
+        $input = stripslashes($input);
+        return $input;
+    }
+
+    if($_POST){
+        $textContent = testInput($_POST['textContent']);
+        $fn = testInput($_POST['fn']);
+        $isVisible = true; //не го взимаме от форма...
+
+        if(!$textContent){
+            $errors[] = "Не сте въвели коментар!";
+        }
+        if(!is_numeric($fn) || strlen($fn) != 5){
+            $errors[] = "Некоректен факултетен номер!";
+        }
+
+        if($textContent && strlen($fn) == 5 && is_numeric($fn)){
+            $comment = new Comment($textContent, $fn, $isVisible);
+            $exists = $comment->exists();
+            if($exists["successfullyExecuted"] == false){
+                $errors[] = "Неуспешна заявка - error message: " . $exists["errMessage"];
+            }
+            else if($exists["successfullyExecuted"] == true){
+                if($exists["commentExists"] == true){
+                    $errors[] = "Коментарът вече е въведен!";
+                }
+                else if($exists["commentExists"] == false){
+                    $create = $comment->createComment();
+                    if($create["successfullyExecuted"] == false){
+                        $errors[] = "Неуспешна заявка за добавяне в базата данни - error message: " . $create["errMessage"];
+                    }
+                }
+            }
+        }
+
+        if($errors) {
+            foreach ($errors as $error) {
+                echo $error;
+                echo "<br/>";
+            }
+            // echo "<a href='register.php'>Кликни тук, за да се върнеш към формата</a>";
+        } else {
+            echo "Събитието е добавено успешно!";
+            echo "<br>";
+            // echo "<a href='register.php'>Кликни тук, за да се върнеш към формата</a>";
+        }
+
+    }
+    else{
+        echo "Невалиден тип заявка!";
+    }
+
+?>
