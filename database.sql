1668ec45b9f8623e4d2f6af9d348804ff8dc5bdd
nikolay <nikolayynayydenov@gmail.com>
2021-01-19 11:00:26 +0200

    modify events table

--- database.sql
+++ database.sql
@@ -1,83 +1,168 @@
--- --------------------------------------------------------
--- Хост:                         localhost
--- Версия на сървъра:            10.4.12-MariaDB-log - mariadb.org binary distribution
--- ОС на сървъра:                Win64
--- HeidiSQL Версия:              11.0.0.5919
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 19, 2021 at 09:59 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `web-technologies`
+--
 
--- Дъмп на структурата на БД web-technologies
-DROP DATABASE `web-technologies`;
-CREATE DATABASE IF NOT EXISTS `web-technologies` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `web-technologies`;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `attendance`
+--
 
--- Дъмп структура за таблица web-technologies.teachers
-CREATE TABLE IF NOT EXISTS `teachers` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `attendance` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `faculty_number` mediumint(8) UNSIGNED NOT NULL,
+  `logged_at` datetime DEFAULT NULL,
+  `event_id` int(10) UNSIGNED NOT NULL,
+  `thrust` tinyint(3) UNSIGNED DEFAULT NULL,
+  `check_description` varchar(50) DEFAULT NULL,
+  `enroll_source` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `content` text NOT NULL,
+  `event_id` int(10) UNSIGNED NOT NULL,
+  `is_visible` tinyint(4) DEFAULT 1,
+  `faculty_number` mediumint(8) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text DEFAULT NULL,
+  `date` date NOT NULL,
+  `start` time DEFAULT NULL,
+  `end` time DEFAULT NULL,
+  `teacher_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `id` int(10) UNSIGNED NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `last_name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Дъмп структура за таблица web-technologies.events
-CREATE TABLE IF NOT EXISTS `events` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `description` text DEFAULT NULL,
-  `date` datetime NOT NULL,
-  `start` datetime DEFAULT NULL,
-  `end` datetime DEFAULT NULL,
-  `teacher_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_events_teacher_id_teachers_id` (`teacher_id`),
-  CONSTRAINT `FK_events_teacher_id_teachers_id` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Indexes for dumped tables
+--
 
--- Дъмп структура за таблица web-technologies.comments
-CREATE TABLE IF NOT EXISTS `comments` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `content` text NOT NULL,
-  `event_id` int(10) unsigned NOT NULL,
-  `is_visible` tinyint(4) DEFAULT 1,
-  `faculty_number` mediumint(8) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_event_id_events_id` (`event_id`),
-  CONSTRAINT `FK_event_id_events_id` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_attendance_event_id_events_id` (`event_id`);
 
--- Изнасянето на данните беше деселектирано.
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_event_id_events_id` (`event_id`);
 
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_events_teacher_id_teachers_id` (`teacher_id`);
 
+--
+-- Indexes for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`id`);
 
--- Дъмп структура за таблица web-technologies.attendance
-CREATE TABLE IF NOT EXISTS `attendance` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `faculty_number` mediumint(8) unsigned NOT NULL,
-  `logged_at` datetime DEFAULT NULL,
-  `event_id` int(10) unsigned NOT NULL,
-  `thrust` tinyint(3) unsigned DEFAULT NULL,
-  `check_description` varchar(50) DEFAULT NULL,
-  `enroll_source` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_attendance_event_id_events_id` (`event_id`),
-  CONSTRAINT `FK_attendance_event_id_events_id` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
--- Изнасянето на данните беше деселектирано.
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
--- Изнасянето на данните беше деселектирано.
+--
+-- AUTO_INCREMENT for table `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `FK_attendance_event_id_events_id` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`);
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `FK_event_id_events_id` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`);
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `FK_events_teacher_id_teachers_id` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
