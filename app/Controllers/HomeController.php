b74136695aebdc3b457af8cda002be7ed9a1a83c
nikolay <nikolayynayydenov@gmail.com>
2021-01-25 18:47:16 +0200

    redistribute logic

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -10,4 +10,26 @@ class HomeController
         view('home');
     }
 
+    public function frontPage_method()
+    {
+        view('front_page');
+    }
+
+    public function showDashboard()
+    {
+
+        $events = [1,2,3,4,5,6];
+        view('dashboard', [
+            'events' => $events
+        ]); 
+
+    }
+
+    
+
+    public function dashboard_method()
+    {
+        view('dashboard');
+    }
+
 }
--- app/routes.php
+++ app/routes.php
@@ -5,11 +5,12 @@ use Core\Router;
 $router = new Router();
 
 // Authentication
-$router->get('/login', 'UsersController@showLogin');
-$router->post('/login', 'UsersController@login_method');
-$router->get('/register', 'UsersController@showRegister');
-$router->post('/register', 'UsersController@register_method');
-$router->post('/enter-teacher', 'UsersController@enterTeacher');
+$router->get('/login', 'AuthController@showLogin');
+$router->post('/login', 'AuthController@login_method');
+$router->get('/register', 'AuthController@showRegister');
+$router->post('/enter-teacher', 'AuthController@enterTeacher');
+$router->post('/logout', 'AuthController@register_method');
+
 
 
 // Events
@@ -24,11 +25,12 @@ $router->get('/attendance/check', 'AttendanceController@checkForm');
 $router->get('/attendance', 'AttendanceController@show');
 
 
+
 //Other
 $router->get('/', 'UsersController@showFrontPage');
-$router->post('/front_page', 'UsersController@frontPage_method');
-$router->get('/dashboard', 'UsersController@showDashboard');
-$router->post('/dashboard', 'UsersController@dashboard_method');
+$router->post('/front_page', 'HomeController@frontPage_method');
+$router->get('/dashboard', 'HomeController@showDashboard');
+$router->post('/dashboard', 'HomeController@dashboard_method');
 
 
 $router->fallback();
